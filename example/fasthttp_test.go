bc9b4afc414c5caa44a9185a9e7cb65ad583e5e3
elastic7327 <elastic7327@gmail.com>
2018-05-04 17:20:13 +0900

    init

--- example/fasthttp_test.go
+++ example/fasthttp_test.go
@@ -1,7 +1,23 @@
 package example
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/valyala/fasthttp"
+)
 
 func TestFunction(t *testing.T) {
 
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI("https://www.google.com")
+
+	resp := fasthttp.AcquireResponse()
+	client := &fasthttp.Client{}
+
+	client.Do(req, resp)
+
+	bodyBytes := resp.Body()
+
+	fmt.Println(string(bodyBytes))
 }
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+
+	path := ctx.Path()
+	fmt.Println(string(path))
+	ipaddr := ctx.RemoteIP()
+
+	ctx.Write([]byte("Hello World"))
+	fmt.Fprintf(ctx, "Hi there! RequestURI is %q %s", ctx.RequestURI(), ipaddr)
+
+}
 
 func main() {
-	fmt.Println("Hello Fasthttp!")
+
+	if err := fasthttp.ListenAndServe(":8000", requestHandler); err != nil {
+		log.Fatalf("Error in server %s", err)
+	}
 }
